f7551fcb796bc825d9f65d57122803e216726e7e
98aj <103376356+98aj@users.noreply.github.com>
2023-01-15 21:57:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function convertToRoman(num) {
     };
 
   //your code here
-var roman = '',i;
-  for ( i in obj ) {
-    while ( num >= obj[i] ) {
-      roman += i;
-      num -= obj[i];
-    }
+let result = '';
+  
+  for (key in map) {
+    result += key.repeat(Math.floor(num / map[key]));
+    num %= map[key];
   }
-  return roman;
+  
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
